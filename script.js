3468fc619ecb3e5ee3016a796065447a510e72c2
Aokami-dvlp <emanuele.david3@gmail.com>
2021-10-17 20:40:30 +0200

    Status bars now works!

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const canvas1 = document.querySelector('#p1');
 const p1Image = document.createElement('img');
 const canvas2 = document.querySelector('#p2');
 const p2Image = document.createElement('img');
+const p1Name = document.querySelector('#p1Name');
+const p2Name = document.querySelector('#p2Name');
+let p1HpNumeric = document.querySelector('#p1HpNumeric');
+let p2HpNumeric = document.querySelector('#p2HpNumeric');
 
+// Function to fetch the needed data
 async function generate(){
-    let random = Math.floor(Math.random() * 899) + 1;
+// Randomize fetched pokemon
+    let random = Math.floor(Math.random() * 898) + 1;
     let fetchUrl = `https://pokeapi.co/api/v2/pokemon/${random}/`;
     let imageSrc = `https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/${random}.png`;
     let response = await fetch(fetchUrl);
@@ -13,6 +19,7 @@ async function generate(){
     return {name, stats, imageSrc};
     }
 
+// Class to define the pokemon object
 class Pokemon{
     constructor(genData){
     this.pName = genData.name.toUpperCase();
@@ -25,25 +32,53 @@ class Pokemon{
     this.spd = genData.stats[5].base_stat;
     }}
 
-    const button = document.querySelector('button');
+const button = document.querySelector('button');
 
-    button.addEventListener('click',async function() {
+button.addEventListener('click',async function() {
     button.remove();
+    //Call the function to fetch data
     const genData1 = await generate();
-    const player1 = new Pokemon(genData1);
-    console.log(player1);
-    p1Image.src = player1.sprite;
-    p1Image.alt = player1.pName;
-    canvas1.appendChild(p1Image);
-
     const genData2 = await generate();
+
+    //Pass data to class to build the object
+    const player1 = new Pokemon(genData1);
     const player2 = new Pokemon(genData2);
+
+    console.log(player1);
     console.log(player2);
+
+    //Pass the pokemon name and hp on screen
+    p1Name.textContent = player1.pName;
+    p2Name.textContent = player2.pName;
+    
+    let p1CurrHp = player1.hp;
+    let p1MaxHp = player1.hp;
+    p1HpNumeric.textContent = `${p1CurrHp} / ${p1MaxHp} HP`;
+
+    let p2CurrHp = player2.hp;
+    let p2MaxHp = player2.hp;
+    p2HpNumeric.textContent = `${p2CurrHp} / ${p2MaxHp} HP`;
+    
+    //Show status bars:
+    const p1StatusBar = document.querySelector("#p1Bar");
+    const p2StatusBar = document.querySelector("#p2Bar");
+    p1StatusBar.classList.toggle('hiddenLeft');
+    p2StatusBar.classList.toggle('hiddenRight');
+    
+    //Pass the pokemon images on screen after status bars are showed
+    setTimeout(() => {
+
+    p1Image.src = player1.sprite;
     p2Image.src = player2.sprite;
+
+    p1Image.alt = player1.pName;
     p2Image.alt = player2.pName;
+
+    canvas1.appendChild(p1Image);
     canvas2.appendChild(p2Image);
 
-    const matchName = document.createElement('h1');
-    matchName.textContent = `${player1.pName} VS ${player2.pName}`;
-    screen.appendChild(matchName);
+    }, 1000);
+    
+
 })();
+
